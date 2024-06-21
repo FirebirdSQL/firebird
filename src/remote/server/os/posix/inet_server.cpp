700765870207f2507316f379227267bc3d9eb1cd
Alexander Zhdanov <alexander.zhdanov@red-soft.ru>
2024-06-21 16:33:25 +0300

    Added shutdown handler for server to avoid active connections after termination

--- src/remote/server/os/posix/inet_server.cpp
+++ src/remote/server/os/posix/inet_server.cpp
@@ -115,6 +115,7 @@ const char* TEMP_DIR = "/tmp";
 
 static void set_signal(int, void (*)(int));
 static void signal_handler(int);
+static void shutdown_handler(int);
 
 static TEXT protocol[128];
 static int INET_SERVER_start = 0;
@@ -314,6 +315,10 @@ int CLIB_ROUTINE main( int argc, char** argv)
 		// activate paths set with -e family of switches
 		ISC_set_prefix(0, 0);
 
+		// set shutdown signals handler for listener
+		set_signal(SIGTERM, shutdown_handler);
+		set_signal(SIGINT, shutdown_handler);
+
 		// ignore some signals
 		set_signal(SIGPIPE, signal_handler);
 		set_signal(SIGUSR1, signal_handler);
@@ -507,6 +512,10 @@ int CLIB_ROUTINE main( int argc, char** argv)
 			}
 		}
 
+		// set default handlers for child processes
+		signal(SIGTERM, SIG_DFL);
+		signal(SIGINT, SIG_DFL);
+
 		if (classic)
 		{
 			port = INET_server(channel);
@@ -631,6 +640,23 @@ static void signal_handler(int)
 	++INET_SERVER_start;
 }
 
+static void shutdown_handler(int)
+{
+/**************************************
+ *
+ *	s h u t d o w n _ h a n d l e r
+ *
+ **************************************
+ *
+ * Functional description
+ *	Forward sigterm signal to all child processes.
+ *
+ **************************************/
+
+	kill(-1 * getpid(), SIGTERM);
+
+	exit(FINI_OK);
+}
 
 #ifdef FB_RAISE_LIMITS
 static void raiseLimit(int resource)
