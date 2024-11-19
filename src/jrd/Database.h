9912b8a3cfcb39ef8ac00cb89579319b2a0887a6
Artyom Ivanov <artyom.ivanov@red-soft.ru>
2024-11-19 16:42:14 +0300

    Add more error messages that describe a reason of failed sweep startup

--- src/jrd/Database.h
+++ src/jrd/Database.h
@@ -657,9 +657,8 @@ public:
 
 	// returns true if sweeper thread could start
 	bool allowSweepThread(thread_db* tdbb);
-	// Returns true if sweep could run.
-	// Exception can be thrown if another sweep instance is already running.
-	bool allowSweepRun(thread_db* tdbb);
+	// Throw an exception if sweep cannot be run
+	void initiateSweepRun(thread_db* tdbb);
 	// reset sweep flag and release sweep lock
 	void clearSweepFlags(thread_db* tdbb);
 	// reset sweep starting flag, release thread starting mutex
