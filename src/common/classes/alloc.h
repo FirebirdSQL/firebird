b62a7b3bcbd91ee782eaa6f4af84ce1ab027c6e0
Mark Rotteveel <mark@lawinegevaar.nl>
2025-05-05 12:10:14 +0200

    Extern definition should be inside DEBUG_GDS_ALLOC ifdef

--- src/common/classes/alloc.h
+++ src/common/classes/alloc.h
@@ -387,16 +387,15 @@ inline void operator delete[](void* mem, Firebird::MemoryPool& pool ALLOC_PARAMS
 	MemoryPool::globalFree(mem);
 }
 
-#if __cplusplus >= 201402L
-extern void operator delete(void* mem, std::size_t s) noexcept;
-
-extern void operator delete[](void* mem, std::size_t s) noexcept;
-#endif
-
 #ifdef DEBUG_GDS_ALLOC
 
 extern void operator delete(void* mem) noexcept;
 extern void operator delete[](void* mem) noexcept;
+
+#if __cplusplus >= 201402L
+extern void operator delete(void* mem, std::size_t s) noexcept;
+extern void operator delete[](void* mem, std::size_t s) noexcept;
+#endif
 
 #endif // DEBUG_GDS_ALLOC
 
