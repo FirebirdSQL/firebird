df1717bfbb5034410acc44fc382faf554d9bd789
Dimitry Sibiryakov <sd@ibphoenix.com>
2025-09-09 11:12:53 +0200

    Use HalfStaticArray as demanded

--- src/dsql/DsqlRequests.cpp
+++ src/dsql/DsqlRequests.cpp
@@ -609,9 +609,9 @@ void DsqlDmlRequest::doExecute(thread_db* tdbb, jrd_tra** traHandle,
 					// Create a temp message buffer and try one more receive.
 					// If it succeed then the next record exists.
 
-					std::unique_ptr<UCHAR[]> message_buffer(FB_NEW_POOL(getPool()) UCHAR[outMsgLength]);
+					HalfStaticArray<UCHAR, BUFFER_SMALL> message_buffer(getPool(), outMsgLength);
 
-					JRD_receive(tdbb, request, message->msg_number, outMsgLength, message_buffer.get());
+					JRD_receive(tdbb, request, message->msg_number, outMsgLength, message_buffer.getBuffer(outMsgLength, false));
 
 					// Still active request means that second record exists
 					if ((request->req_flags & req_active))
