108d0fa144f6b6e1eeff68ed1eabb05a5ab95d9b
Bobbert <chemistgmo@gmail.com>
2025-10-05 23:09:00 +0300

    refactor: move ret_code declaration into loop scope

--- src/utilities/guard/guard.cpp
+++ src/utilities/guard/guard.cpp
@@ -181,7 +181,6 @@ int CLIB_ROUTINE main( int argc, char **argv)
 	divorce_terminal(mask);
 
 	time_t timer = 0;
-	int ret_code = NORMAL_EXIT;
 
 	do {
 
@@ -226,6 +225,7 @@ int CLIB_ROUTINE main( int argc, char **argv)
 		}
 
 		// wait for child to die, and evaluate exit status
+       int ret_code = NORMAL_EXIT;
 		bool shutdown_child = true;
 		if (!shutting_down)
 		{
