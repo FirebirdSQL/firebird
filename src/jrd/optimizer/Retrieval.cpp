3c5319a730f28918de9fbb672b5b08fb7e3c932c
Dmitry Yemanov <dyemanov@users.noreply.github.com>
2026-02-17 09:06:51 +0300

    Fix assertion inside InnerJoin::estimateCost()

--- src/jrd/optimizer/Retrieval.cpp
+++ src/jrd/optimizer/Retrieval.cpp
@@ -356,6 +356,8 @@ InversionCandidate* Retrieval::getInversion()
 			&invCandidate->dependentFromStreams);
 	}
 
+	invCandidate->dependencies = invCandidate->dependentFromStreams.getCount();
+
 #ifdef OPT_DEBUG_RETRIEVAL
 	// Debug
 	printFinalCandidate(invCandidate);
