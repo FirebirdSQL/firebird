469505084961bcc2bbaba3b9d547d36b71be308d
Artyom Abakumov <artyom.abakumov@red-soft.ru>
2026-02-27 01:10:40 +0300

    Remove unneccecery namespace scope

--- src/dsql/PackageNodes.h
+++ src/dsql/PackageNodes.h
@@ -148,7 +148,7 @@ public:
 	}
 
 private:
-	CachedResource<Jrd::Constant, Jrd::RoutinePermanent> m_constant;
+	CachedResource<Constant, RoutinePermanent> m_constant;
 	const QualifiedName m_fullName;
 
 	const UCHAR m_itemType;
