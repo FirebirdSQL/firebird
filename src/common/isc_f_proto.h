405d9663bb3e976ac8838d78c465ac62a1aa0407
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2026-03-06 08:27:42 -0300

    WIP common

--- src/common/isc_f_proto.h
+++ src/common/isc_f_proto.h
@@ -37,33 +37,33 @@ namespace Firebird
 enum iscProtocol {ISC_PROTOCOL_LOCAL, ISC_PROTOCOL_TCPIP};
 
 #ifndef NO_NFS
-bool		ISC_analyze_nfs(Firebird::PathName&, Firebird::PathName&);
+bool		ISC_analyze_nfs(PathName&, PathName&);
 #endif
 #ifdef WIN_NT
-bool		ISC_analyze_pclan(Firebird::PathName&, Firebird::PathName&);
+bool		ISC_analyze_pclan(PathName&, PathName&);
 #endif
-bool		ISC_analyze_protocol(const char*, Firebird::PathName&, Firebird::PathName&, const char*, bool needFile);
-bool		ISC_analyze_tcp(Firebird::PathName&, Firebird::PathName&, bool = true);
-bool		ISC_check_if_remote(const Firebird::PathName&, bool);
-iscProtocol	ISC_extract_host(Firebird::PathName&, Firebird::PathName&, bool);
-bool		ISC_expand_filename(Firebird::PathName&, bool);
-void		ISC_systemToUtf8(Firebird::AbstractString& str);
-void		ISC_utf8ToSystem(Firebird::AbstractString& str);
-void		ISC_escape(Firebird::AbstractString& str);
-void		ISC_unescape(Firebird::AbstractString& str);
+bool		ISC_analyze_protocol(const char*, PathName&, PathName&, const char*, bool needFile);
+bool		ISC_analyze_tcp(PathName&, PathName&, bool = true);
+bool		ISC_check_if_remote(const PathName&, bool);
+iscProtocol	ISC_extract_host(PathName&, PathName&, bool);
+bool		ISC_expand_filename(PathName&, bool);
+void		ISC_systemToUtf8(AbstractString& str);
+void		ISC_utf8ToSystem(AbstractString& str);
+void		ISC_escape(AbstractString& str);
+void		ISC_unescape(AbstractString& str);
 
 // This form of ISC_expand_filename makes epp files happy
 inline bool	ISC_expand_filename(const TEXT* unexpanded, USHORT len_unexpanded,
 								TEXT* expanded, FB_SIZE_T len_expanded,
 								bool expand_share)
 {
-	Firebird::PathName pn(unexpanded, len_unexpanded ? len_unexpanded : fb_strlen(unexpanded));
+	PathName pn(unexpanded, len_unexpanded ? len_unexpanded : fb_strlen(unexpanded));
 	ISC_expand_filename(pn, expand_share);
 	// What do I return here if the previous call returns false?
 	return (pn.copyTo(expanded, len_expanded) != 0);
 }
 
-void		ISC_expand_share(Firebird::PathName&);
+void		ISC_expand_share(PathName&);
 int			ISC_file_lock(SSHORT);
 int			ISC_file_unlock(SSHORT);
 
