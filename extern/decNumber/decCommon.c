ca3027e1badf760ead0693b7164f2ceb6118c5dd
ChudaykinAlex <chudaykinalex@gmail.com>
2026-05-06 11:11:33 +0300

    Suggestions for fixing the problem.

--- extern/decNumber/decCommon.c
+++ extern/decNumber/decCommon.c
@@ -461,7 +461,8 @@ static decFloat * decFinalize(decFloat *df, bcdnum *num,
           uByte *t=buffer;              // safe target
           uByte *tlsd=buffer+(ulsd-umsd)+shift; // target LSD
           // printf("folddown shift=%ld\n", (LI)shift);
-          for (; s<=ulsd; s+=4, t+=4) UBFROMUI(t, UBTOUI(s));
+          for (; s+3<=ulsd; s+=4, t+=4) UBFROMUI(t, UBTOUI(s));
+          for (; s<=ulsd; s++, t++) *t=*s;
           for (t=tlsd-shift+1; t<=tlsd; t+=4) UBFROMUI(t, 0);  // pad 0s
           num->exponent-=shift;
           umsd=buffer;
