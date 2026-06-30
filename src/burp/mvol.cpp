f2622a67288c9fb4ca34e4a67db2f03edd8e7e6c
Artyom Abakumov <artem210966@gmail.com>
2026-06-30 19:48:55 +0300

    Merge commit from fork
    * Catch invalid att_backup_blksize in backup header
    
    * Add upper limit check for temp_buffer_size
    
    ---------
    
    Co-authored-by: Artyom Abakumov <artyom.abakumov@red-soft.ru>

--- src/burp/mvol.cpp
+++ src/burp/mvol.cpp
@@ -42,6 +42,7 @@
 #include "../burp/burp.h"
 #include "../burp/burp_proto.h"
 #include "../burp/mvol_proto.h"
+#include "../burp/split/spit.h"
 #include "../yvalve/gds_proto.h"
 #include "../common/gdsassert.h"
 #include "../common/os/os_utils.h"
@@ -72,6 +73,7 @@ const char* TERM_OUTPUT	= "/dev/tty";
 #endif
 
 const int MAX_HEADER_SIZE	= 512;
+const ULONG MAX_BUFSIZE		= 32 * M_BYTES; // Theoretical limit. May be increased in case of regression
 
 static inline int get(BurpGlobals* tdgbl)
 {
@@ -1155,6 +1157,10 @@ static bool read_header(DESC handle, ULONG* buffer_size, USHORT* format, bool in
 		case att_backup_blksize:
 			{
 				const ULONG temp_buffer_size = get_numeric();
+				if (temp_buffer_size == 0 || temp_buffer_size > MAX_BUFSIZE)
+					BURP_error(267, true, tdgbl->mvol_old_file);
+					// msg 267 backup file %s might be corrupt
+
 				if (init_flag)
 					*buffer_size = temp_buffer_size;
 			}
