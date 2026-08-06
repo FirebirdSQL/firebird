2e8d5b95765f29ebfad8318ce47a0f0453eebff9
Foxpunk <foxpunk1231@gmail.com>
2026-08-06 17:41:01 +0300

    do static_cast<size_t> for len

--- src/jrd/trace/TraceConfigStorage.cpp
+++ src/jrd/trace/TraceConfigStorage.cpp
@@ -179,7 +179,7 @@ void ConfigStorage::mutexBug(int state, const char* string)
 {
 	TEXT msg[BUFFER_TINY];
 	[[maybe_unused]] const int len = snprintf(msg, sizeof(msg), "ConfigStorage: mutex %s error, status = %d", string, state);
-	fb_assert(len >= 0 && len < static_cast<int>(sizeof(msg)));
+	fb_assert(len >= 0 && static_cast<size_t>(len) < sizeof(msg));
 	fb_utils::logAndDie(msg);
 }
 
