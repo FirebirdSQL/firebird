8e49e05135b6214d5d3d1c97233fae08436ead65
GitHub Action <action@github.com>
2026-09-02 22:20:36 +0000

    increment build number

--- src/jrd/build_no.h
+++ src/jrd/build_no.h
@@ -3,16 +3,16 @@
                *** DO NOT EDIT ***
   TO CHANGE ANY INFORMATION IN HERE PLEASE
   EDIT src/misc/writeBuildNum.sh
-  FORMAL BUILD NUMBER:2166
+  FORMAL BUILD NUMBER:2167
 */
 
-#define PRODUCT_VER_STRING "6.0.0.2166"
-#define FILE_VER_STRING "WI-T6.0.0.2166"
-#define LICENSE_VER_STRING "WI-T6.0.0.2166"
-#define FILE_VER_NUMBER 6, 0, 0, 2166
+#define PRODUCT_VER_STRING "6.0.0.2167"
+#define FILE_VER_STRING "WI-T6.0.0.2167"
+#define LICENSE_VER_STRING "WI-T6.0.0.2167"
+#define FILE_VER_NUMBER 6, 0, 0, 2167
 #define FB_MAJOR_VER "6"
 #define FB_MINOR_VER "0"
 #define FB_REV_NO "0"
-#define FB_BUILD_NO "2166"
+#define FB_BUILD_NO "2167"
 #define FB_BUILD_TYPE "T"
 #define FB_BUILD_SUFFIX "Firebird 6.0 Initial"
